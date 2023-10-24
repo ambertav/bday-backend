756b99d02f2335a911fc531f52547456c14f4ad6
Amber Taveras <taveras.amber@gmail.com>
2023-10-24 19:38:01 -0400

    adds tags retrieve and tests

---
+++ controllers/tagController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'; 
+import mongoose from 'mongoose';
+import Tag, { IFriendDocument } from '../models/friend';
+import { IExtReq } from '../interfaces/auth';
+
+
+export async function getTags (req : Request & IExtReq, res : Response) {
+    try {
+        const tags = await Tag.find({});
+        if (!tags) return res.status(404).json({ message: 'Tags not found'});
+
+        return res.status(200).json(tags);
+    } catch (error : any) {
+        return res.status(500).json({
+            error: error.message
+        });
+    }
+}
