660ceafbb6b91608af1ee59b99544684eeee51fb
cango91 <can.gologlu@gmail.com>
2023-10-25 12:15:31 -0400

    update sys prompt

--- controllers/giftRecommendationController.ts
+++ controllers/giftRecommendationController.ts
@@ -11,10 +11,11 @@ Generate gift recommendations based on input.
 + You must only and exactly reply in the desired OUTPUT FORMAT. OUTPUT must include 5 gift ideas. Only reply as an array of JSON Objects as described in OUTPUT FORMAT
 **INPUT FORMAT**
 {
-    "bio": "(optional) a short description of the person the gift is for",
-    "giftPreferences": "(optional) what types of gifts the person preferes. i.e. present, experience, donation, etc.",
-    "tags": ["an array of short tags desciribing the person, can be anything from gender to aesthetics to generic tags"],
+    "giftPreferences": "(required) what types of gifts the person preferes. i.e. present, experience, donation, etc.",
+    "tags": ["(required)  an array of short tags desciribing the person, can be anything from gender to aesthetics to generic tags"],
     "budget": "the maximum ballpark cost of the gift recommendation",
+    "age": "(required)  age of the person the gift is for",
+"gender": "(required) gender of the person the gift is for"
 }
 **OUTPUT FORMAT**
 [
@@ -24,13 +25,12 @@ Generate gift recommendations based on input.
         "imageSearchQuery": "a simple short query to search for a thumbnail image representative of this gift idea",
     }
 ]`
-
 export async function recommendGift(req: Request & IExtReq, res: Response) {
     try {
         const friend = await Friend.findById(req.params.id);
         if(!friend) throw {status: 404, message: "Friend not found"};
         let {tags, giftTypes, budget} = req.body;
-        
+
     } catch (error: any) {
         if ('status' in error && 'message' in error) {
             sendError(res, error as HTTPError);
