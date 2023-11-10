90cca97b2b680da6d7157e25418e276c729cdc0d
Amber Taveras <taveras.amber@gmail.com>
2023-11-10 12:40:37 -0500

    organizes and selects only necessary tags for frontend

--- modules/tags/tagController.ts
+++ modules/tags/tagController.ts
@@ -1,13 +1,41 @@
 import { Request, Response } from 'express'; 
-import mongoose from 'mongoose';
 import Tag from './models/tag';
 import { IExtReq } from '../../interfaces/auth';
 
 
-export async function getTags (req : Request & IExtReq, res : Response) {
+export async function getDefaultTags (req : Request & IExtReq, res : Response) {
     try {
-        const tags = await Tag.find({});
-        if (!tags) return res.status(404).json({ message: 'Tags not found'});
+        const tags = await Tag.aggregate([
+            {
+                $match: { type: { $ne: 'custom' } } // only retrieving default / non custom tags
+            },
+            {
+                $group: {
+                    _id: '$type', // grouping by the type
+                    tags: {
+                        $push: {
+                            _id: '$_id',
+                            title: '$title'
+                        }
+                    }
+                }
+            },
+            {
+                $addFields: { section: '$_id' } // adding section to rename _id so that its clearer when rendering
+            },
+            {
+                $sort: { section: -1 } // sort by section to retain consistent order
+            },
+            {
+                $project: { 
+                    _id: 0,
+                    tags: 1,
+                    section: 1
+                }
+            }
+        ]);
+
+        if (!tags || tags.length === 0) return res.status(404).json({ message: 'Tags not found'});
 
         return res.status(200).json(tags);
     } catch (error : any) {
@@ -16,3 +44,4 @@ export async function getTags (req : Request & IExtReq, res : Response) {
         });
     }
 }
+
--- modules/tags/tagsRoute.ts
+++ modules/tags/tagsRoute.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import * as tagCtrl from './tagController';
 const router = Router();
 
-router.get('/', tagCtrl.getTags);
+router.get('/', tagCtrl.getDefaultTags);
 
 
 export default router;
