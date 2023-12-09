401b466d219384953492786ab79d744601027137
Amber Taveras <taveras.amber@gmail.com>
2023-12-08 20:12:33 -0500

    uses agenda to run tag cleanup

--- modules/tags/tagController.ts
+++ modules/tags/tagController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'; 
 import Tag from './models/tag';
 import { IExtReq } from '../../interfaces/auth';
+import Agenda from "agenda";
 
 
 export async function getDefaultTags (req : Request & IExtReq, res : Response) {
@@ -65,4 +66,54 @@ export async function getTagSuggestions (req : Request & IExtReq, res : Response
             error: error.message
         });
     }
+}
+
+export async function cleanTags () {
+    try {
+        const orphanAggregation = await Tag.aggregate([
+            {
+                $lookup: {
+                    from: 'friends',
+                    // matching referenced tag ids in friends collection with ids in tag collection
+                    localField: '_id', 
+                    foreignField: 'tags',
+                    // creating array on tag documents containing friends that reference it
+                    as: 'taggedFriends'
+                }
+            },
+            {
+                // filtering to return tags in which the array is empty (the tags that are not referenced)
+                $match: { taggedFriends: { $size: 0 }, type: 'custom' } 
+            },
+            {
+                $project: {
+                    _id: 1 // using just the id field
+                }
+            }
+        ]);
+
+        // if no orphans found, return
+        if (orphanAggregation.length === 0) return console.log('No orphaned tags found');
+
+        // convert to array with ids only
+        const orphanTags = Array.from(orphanAggregation).map(tag => tag._id.toString());
+        console.log(orphanTags);
+
+        const deletedTags = await Tag.deleteMany({ _id: { $in: orphanTags }}); // delete all the tags in array
+        return console.log(`${deletedTags.deletedCount} orphaned tags deleted`);
+        
+    } catch (error : any) {
+        console.error('Error occured while cleaning tags collection: ', error.message);
+    }
+}
+
+export async function startTagAgenda () {
+    const agenda = new Agenda({ db: { address: process.env.DATABASE_URL!, collection: 'Jobs' } });
+    agenda.define('clean out orphaned tags', async () => {
+        console.log("running tag cleanup");
+        await cleanTags();
+        console.log('Done');
+    });
+    await agenda.start();
+    await agenda.every('24 hours', 'clean out orphaned tags');
 }
