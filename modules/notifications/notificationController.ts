3addc63603ad7d3f9882701b761aefd2575e88ec
Amber Taveras <taveras.amber@gmail.com>
2023-12-18 19:04:46 -0500

    updates aggreg pipeline to pop friend and sort by friend dob

--- modules/notifications/notificationController.ts
+++ modules/notifications/notificationController.ts
@@ -9,22 +9,37 @@ export async function getNotifications (req : Request & IExtReq, res : Response)
     try {
         const notifications = await Notification.aggregate([
             {
-                $match: { user: new mongoose.Types.ObjectId(req.user!) } // find notifications for user
+                $match: { user: new mongoose.Types.ObjectId(req.user!) }
             },
             {
-                $sort: { isRead: -1, createdAt: 1 } // sort so that isRead: false and soonest creation are first
-            },
-            {
-                $project: { // only need isRead and friend info
-                    friend: 1,
-                    isRead: 1,
+                $lookup: {
+                    from: 'friends',
+                    localField: 'friend',
+                    foreignField: '_id',
+                    as: 'friendData'
                 }
             },
+            {
+                $unwind: '$friendData'
+            },
+            {
+                $sort: { isRead: -1, 'friendData.dob': 1, createdAt: 1 }
+            },
+            {
+                $project: {
+                    friend: {
+                        _id: '$friendData._id',
+                        name: '$friendData.name',
+                        dob: '$friendData.dob',
+                        photo: '$friendData.photo'
+                    },
+                    isRead: 1
+                }
+            }
         ]);
-
-
+    
+        
         if (notifications.length > 0) { // if notifications...
-            await Notification.populate(notifications, { path: 'friend', select: '_id name dob photo'}); // populate friend info
             // find user's timezone for daysUntilBirthday calculation
             const userProfile = await UserProfile.findOne({ user: req.user }).select('timezone');
             let timezone = userProfile?.timezone;
@@ -34,8 +49,8 @@ export async function getNotifications (req : Request & IExtReq, res : Response)
             const { current, past } = notifications.reduce((result, n) => {
                 const days = daysFromBirthday(n.friend.dob, timezone!);
 
-                if (n.isRead === true) result.past.push({ ...n, friend: { ...n.friend.toJSON(), daysUntilBirthday: days }});
-                else if (n.isRead === false) result.current.push({ ...n, friend: { ...n.friend.toJSON(), daysUntilBirthday: days }});
+                if (n.isRead === true) result.past.push({ ...n, friend: { ...n.friend, daysUntilBirthday: days }});
+                else if (n.isRead === false) result.current.push({ ...n, friend: { ...n.friend, daysUntilBirthday: days }});
 
                 return result;
 
