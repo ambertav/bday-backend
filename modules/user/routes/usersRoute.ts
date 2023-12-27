581f3a0c3b5a923015395ed640632ec4e943082a
Amber Taveras <taveras.amber@gmail.com>
2023-12-27 01:52:35 -0500

    updated send verif. email adds verify user

--- modules/user/routes/usersRoute.ts
+++ modules/user/routes/usersRoute.ts
@@ -10,5 +10,7 @@ router.put("/password", requireLogin, usersCtrl.updatePassword);
 router.put("/", requireLogin, usersCtrl.updateUserDetails);
 router.delete("/", requireLogin, usersCtrl.deleteUser);
 router.post("/confirm-delete", requireLogin, usersCtrl.confirmDeleteUser);
+router.post('/verify-email', usersCtrl.verifyEmail);
+router.post('/resend-email', usersCtrl.resendEmail);
 
 export default router;
--- modules/user/services/verificationService.ts
+++ modules/user/services/verificationService.ts
@@ -1,10 +1,12 @@
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import { createJwt } from "./tokenService";
-import { sendMail } from "../../../utilities/emailService";
 import { SendMailOptions } from 'nodemailer';
+import { sendMail } from "../../../utilities/emailService";
+import { FRONTEND_BASE_URL } from '../../../utilities/constants';
 import User from '../models/user';
 
 
-const { EMAIL_SECRET, EMAIL_USER } = process.env;
+const { EMAIL_SECRET, EMAIL_USER, EMAIL_JWT_EXPIRE } = process.env;
 
 export async function sendEmailVerification (id : string) {
     try {
@@ -13,10 +15,18 @@ export async function sendEmailVerification (id : string) {
         if (!user) throw new Error('User not found');
 
         // creates token for verification
-        const emailToken = createJwt(user._id, EMAIL_SECRET, '1d');
+        const emailToken = createJwt(
+            { 
+                sub: user._id, // for user identification
+                verified: user.verified, // 
+                updatedAt: user.updatedAt!.getTime() // to ensure single use by checking if token updatedAt value matches database 
+            }, 
+            EMAIL_SECRET, 
+            EMAIL_JWT_EXPIRE,
+        );
 
         // frontend url
-        const url = `http://localhost:3000/verify-email/${emailToken}`;
+        const url = `${FRONTEND_BASE_URL}/verify-email?et=${emailToken}`;
 
         const mailOptions : SendMailOptions = { // email info
             from: `Presently 🎁 <${EMAIL_USER}>`,
@@ -29,6 +39,29 @@ export async function sendEmailVerification (id : string) {
 
         // if result.messageId, then successfully send email
         return result.messageId || null;
+
+    } catch (error : any) {
+        console.error(error);
+        throw error;
+    }
+}
+
+export async function verifyUserEmail (decodedToken : JwtPayload) {
+    try {
+        // find user
+        console.log(decodedToken);
+        const user = await User.findById(decodedToken.payload.sub);
+        if (!user) throw new Error('User not found');
+
+        // if user was updated between time of token creation and token utilization, abort and have to generate new token
+        if (user.updatedAt!.getTime() !== decodedToken.payload.updatedAt) throw new Error('This token is invalid. Please try again');
+
+        // update user and save changes
+        user.verified = true;
+        await user.save();
+
+        // return success message
+        return 'User\'s email was verified successfully';
 
     } catch (error : any) {
         console.error(error);
