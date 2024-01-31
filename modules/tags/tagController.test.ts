0629795b553201ea640a187faa906b7c5d69b19e
Amber Taveras <taveras.amber@gmail.com>
2024-01-31 00:08:43 -0500

    adds tests for tag suggestions

--- modules/tags/tagController.test.ts
+++ modules/tags/tagController.test.ts
@@ -3,7 +3,8 @@ import request from 'supertest';
 import { configureApp } from '../../index';
 import bearer from '../../middleware/bearer';
 
-import Tag from './models/tag';
+import Tag, { ITag }from './models/tag';
+import { isTypedArray } from 'util/types';
 
 const app = configureApp([bearer]);
 
@@ -17,6 +18,7 @@ beforeAll(async () => {
 });
 
 afterAll(async () => {
+    await Tag.deleteMany({ title: { $regex: 'suggestion' } });
     await mongoose.connection.close();
 });
 
@@ -24,6 +26,7 @@ afterAll(async () => {
 describe('GET /api/tags/', () => {
     it('should return all default tags', async () => {
         const count = await Tag.countDocuments({ type: { $ne: 'custom' } });
+
         const response = await request(app)
             .get('/api/tags/')
             .expect(200);
@@ -32,3 +35,31 @@ describe('GET /api/tags/', () => {
     });
 });
 
+describe('GET /api/tags/suggestions/', () => {
+    it('should return an array of tags matching search term', async () => {
+        const suggestionTags = await Tag.insertMany([{ title: 'suggestion1' }, { title: 'suggestion2' }]);
+
+        const searchTerm = 'suggestion'
+
+        const response = await request(app)
+            .get(`/api/tags/suggestions?search=${searchTerm}`)
+            .expect(200);
+
+        expect(response.body).toHaveLength(suggestionTags.length);
+
+        response.body.forEach((tag : ITag) => {
+            expect(tag).toHaveProperty('_id');
+            expect(tag.title).toContain(searchTerm);
+        });
+    });
+
+    it('should return message if no matching tags', async () => {
+        const searchTerm = 'invalidTag';
+
+        const response = await request(app)
+        .get(`/api/tags/suggestions?search=${searchTerm}`)
+        .expect(200);
+
+        expect(response.body.message).toBe('No suggested tags');
+    });
+});
