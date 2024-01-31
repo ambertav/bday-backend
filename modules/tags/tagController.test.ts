789fc930b5cfd661add440174b87147e8e007c70
Amber Taveras <taveras.amber@gmail.com>
2024-01-31 00:11:56 -0500

    adds comments

--- modules/tags/tagController.test.ts
+++ modules/tags/tagController.test.ts
@@ -25,28 +25,33 @@ afterAll(async () => {
 
 describe('GET /api/tags/', () => {
     it('should return all default tags', async () => {
+        // counts all tags that are not custom
         const count = await Tag.countDocuments({ type: { $ne: 'custom' } });
 
         const response = await request(app)
             .get('/api/tags/')
             .expect(200);
 
+        // compares count to ensure that all non custom tags are being returned
         expect(response.body.length).toEqual(count);
     });
 });
 
 describe('GET /api/tags/suggestions/', () => {
     it('should return an array of tags matching search term', async () => {
+        // inserts tags to search
         const suggestionTags = await Tag.insertMany([{ title: 'suggestion1' }, { title: 'suggestion2' }]);
 
-        const searchTerm = 'suggestion'
+        const searchTerm = 'suggestion';
 
         const response = await request(app)
             .get(`/api/tags/suggestions?search=${searchTerm}`)
             .expect(200);
 
+        // matching length of return to tags for search
         expect(response.body).toHaveLength(suggestionTags.length);
 
+        // ensures that return has titles that match searchTerm
         response.body.forEach((tag : ITag) => {
             expect(tag).toHaveProperty('_id');
             expect(tag.title).toContain(searchTerm);
@@ -54,11 +59,11 @@ describe('GET /api/tags/suggestions/', () => {
     });
 
     it('should return message if no matching tags', async () => {
-        const searchTerm = 'invalidTag';
+        const searchTerm = 'invalidSearch';
 
         const response = await request(app)
-        .get(`/api/tags/suggestions?search=${searchTerm}`)
-        .expect(200);
+            .get(`/api/tags/suggestions?search=${searchTerm}`)
+            .expect(200);
 
         expect(response.body.message).toBe('No suggested tags');
     });
